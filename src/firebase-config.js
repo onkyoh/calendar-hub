6aa7ef57f14e564672cc7c329453f77333722987
onkyoh <adnanradwan_8@hotmail.com>
2023-01-22 23:01:49 -0500

    removed messaging token

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,7 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from '@firebase/firestore';
 import { getAuth } from "firebase/auth";
-import { getMessaging } from "firebase/messaging";
 
 
 const firebaseConfig = {
@@ -18,4 +17,3 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-export const messaging = getMessaging(app);
